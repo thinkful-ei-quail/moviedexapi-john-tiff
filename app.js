0a3519f6ae0861ce3d17e1f9fe7b2661f4e07b4a
Tiffany <tiffanysummerford@gmail.com>
2020-08-11 16:11:49 -0400

    nicee

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const morgan = require('morgan')
 const helmet = require('helmet')
-const cors= require('cors')
+const cors = require('cors')
 const MOVIES = require('./movies-data.json')
 
 const app = express()
@@ -15,23 +15,27 @@ app.use(function validateBearerToken(req, res, next) {
     const authMethod = req.get('Authorization')
 
     if (!authMethod || !authMethod.toLowerCase().startsWith("bearer ")) {
-        res.status(401).json({ error: 'Unauthorized access, please provide proper authentication'})
+        res.status(401).json({ error: 'Unauthorized access, please provide proper authentication' })
     }
     next()
 })
 
 app.get('/movie', (req, res) => {
-    const {genre, country, avg_vote} = req.query
+    const { genre, country, avg_vote } = req.query
     let results = MOVIES
 
+    if (avg_vote) {
+        if (avg_vote > 0 && avg_vote < 9.6) {
+            results = results.filter((movie) => movie.avg_vote >= avg_vote)
+        } else {
+            ('Sorry, please provide a rating between 0 and 9.5 :/')
+        }
+    }
     if (genre) {
         results = results.filter((movie) => movie.genre.toLowerCase().includes(genre.toLowerCase()))
     }
     if (country) {
         results = results.filter((movie) => movie.country.toLowerCase().includes(country.toLowerCase()))
-    }
-    if (avg_vote) {
-        results = results.filter((movie) => movie.avg_vote >= avg_vote)
     }
     res.json(results)
 })
