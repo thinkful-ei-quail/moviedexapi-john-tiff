2b267321832a45e546fd0872343b0dc0290c5205
Tiffany <tiffanysummerford@gmail.com>
2020-08-11 16:18:58 -0400

    Deleted instructions from app.js and using commit msg for first time p cool

--- app.js
+++ app.js
@@ -43,12 +43,3 @@ app.get('/movie', (req, res) => {
 const server = app.listen(8080, () => {
     console.log(`Port ${server.address().port} is an avocado`)
 })
-// Users can search for Movies by genre, country or avg_vote
-// The endpoint is GET /movie
-// The search options for genre, country, and/or average vote are provided in query string parameters.
-// When searching by genre, users are searching for whether the Movie's genre includes a specified string. The search should be case insensitive.
-// When searching by country, users are searching for whether the Movie's country includes a specified string. The search should be case insensitive.
-// When searching by average vote, users are searching for Movies with an avg_vote that is greater than or equal to the supplied number.
-// The API responds with an array of full movie entries for the search results
-// The endpoint only responds when given a valid Authorization header with a Bearer API token value.
-// The endpoint should have general security in place such as best practice headers and support for CORS.
